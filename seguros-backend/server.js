3043c27f5340b4dad4f05af946bb17f354cee8ce
Joseph Chachalo <3robtichachalojoseph@gmail.com>
2025-07-02 00:47:09 -0500

    Oh my lord

--- seguros-backend/server.js
+++ seguros-backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Middleware CORS manual para permitir PATCH
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://proyecto-seguros-ucb5.vercel.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
